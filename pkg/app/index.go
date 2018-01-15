480e8e35014837da4f629d868e42d6fdc57c9045
Stp.Develo <stp.develo@gmail.com>
2018-01-15 21:25:05 +0700

    change package set new function for render

--- pkg/app/index.go
+++ pkg/app/index.go
@@ -3,9 +3,9 @@ package app
 import (
 	"net/http"
 
-	"github.com/stpdevelo/go-simple/pkg/view"
+	"github.com/stpdevelo/go-simple-project/pkg/view"
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	view.Index(w, nil)
+	view.Render(w, view.TmplIndex, nil)
 }
