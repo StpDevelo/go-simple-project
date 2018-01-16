23591b30ffd056f624b6cf8115d43c0f0005fa43
Stp.Develo <stp.develo@gmail.com>
2018-01-17 00:30:39 +0700

    edit methods for config file

--- pkg/view/view.go
+++ pkg/view/view.go
@@ -3,16 +3,33 @@ package view
 import (
 	"net/http"
 
+	"github.com/StpDevelo/go-simple-project/pkg/app"
 	"github.com/acoshift/header"
 )
 
+type view struct {
+	baseURL string
+}
+
+// Config is the view config
+type Config struct {
+	BaseURL string
+}
+
+// New Create new view package
+func New(config Config) app.View {
+	return &view{
+		baseURL: config.BaseURL,
+	}
+}
+
 // Index Render indexPage
-func Index(w http.ResponseWriter, r *http.Request) {
+func (v *view) Index(w http.ResponseWriter, r *http.Request) {
 	render(w, tmplIndex, nil)
 }
 
 // NotFound Render not found page
-func NotFound(w http.ResponseWriter, r *http.Request) {
+func (v *view) NotFound(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set(header.XContentTypeOptions, "nosniff")
 	renderWithStatusCode(w, http.StatusNotFound, tmplNotFound, nil)
 }
